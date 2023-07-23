560896b2aea8b54b18164a35ea659db69c022be3
gerhardt <39679794+Grogu42@users.noreply.github.com>
2023-07-23 21:45:33 +0200

    add solution to task 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+    let sum = 0;
+    let start = 0;
+    let end = 0;
+    if (firstNum < 0 || secondNum < 0) return "ERROR"
+    if ((typeof firstNum != "number") || (typeof secondNum != "number")) return "ERROR"
+    if (firstNum <= secondNum) {
+        start = firstNum;
+        end = secondNum;
+    }
+    else {
+        end = firstNum;
+        start = secondNum;
+    }
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
